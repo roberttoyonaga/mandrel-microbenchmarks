437c61c26df30f313ec6de2f0713c52691fb4bde
Robert Toyonaga <rtoyonag@redhat.com>
2024-11-22 14:56:16 -0500

    improve benchmark

--- src/main/java/org/sample/Benchmarks.java
+++ src/main/java/org/sample/Benchmarks.java
@@ -16,16 +16,11 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.infra.Blackhole;
 
-import io.netty.handler.codec.DefaultHeaders;
-import io.netty.handler.codec.http.DefaultHttpHeadersFactory;
-import io.vertx.core.http.impl.HttpUtils;
-
 import io.vertx.core.http.impl.headers.HeadersMultiMap;
 import io.vertx.core.http.HttpHeaders;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.ByteBuf;
@@ -38,6 +33,8 @@ public class Benchmarks {
     private CharSequence[] headerValues;
     public static final DateFormat DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyyy HH:mm:ss z");
 
+    private ByteBuf buf;
+    private HeadersMultiMap hmm;
 
     @Setup
     public void setup() {
@@ -51,24 +48,55 @@ public class Benchmarks {
         headerValues[2] = HttpHeaders.createOptimized("vert.x");
         headerNames[3] = HttpHeaders.DATE;
         headerValues[3] = HttpHeaders.createOptimized(DATE_FORMAT.format(new java.util.Date(0)));
-        }
 
-
-    @Benchmark
-    @Fork(value = 1, warmups = 1)
-    @BenchmarkMode(Mode.Throughput)
-    public void headersMultiMapAdd(Blackhole blackhole) {
-        HeadersMultiMap hmm = HeadersMultiMap.httpHeaders();
+        buf =  PooledByteBufAllocator.DEFAULT.directBuffer();
+        hmm = HeadersMultiMap.httpHeaders();
         for (int i=0; i < headerNames.length; i++) {
-            blackhole.consume(hmm.add(headerNames[i], headerValues[i]));
+            hmm.add(headerNames[i], headerValues[i]);
         }
     }
 
+
     @Benchmark
-    @Fork(value = 1, warmups = 1)
+//    @Fork(value = 1, warmups = 0)
+//    @Measurement(iterations = 20)
+//    @Warmup(iterations = 1000)
     @BenchmarkMode(Mode.Throughput)
-    public ByteBuf pooledByteBufAllocatorNewDirectBuffer(Blackhole blackhole) {
-        ByteBuf buf =  PooledByteBufAllocator.DEFAULT.directBuffer(); // calls newDirectBuffer
+    public HeadersMultiMap headersMultiMapAdd() {
+        HeadersMultiMap hmm = HeadersMultiMap.httpHeaders();
+        int loopSize = headerNames.length;
+        /* We don't want loop unrolling. It allows the optimizations to be too specific to each header due to easier branch prediction.
+        And it means that each version of the method inlined has it's branches hit with a different probability.
+         So we won't get performance representative to what happens in a real Quarkus app with ambiguous headers.
+          -XX:LoopUnrollLimit=1
+         */
+        for (int i=0; i < loopSize; i++) {
+            hmm.clear();// Franz said we may need to clear to avoid the size of the object becoming a relevant factor. Profile to check if clear() becomes relevant.
+           hmm.add(headerNames[i], headerValues[i]);
+        }
+        return hmm;
+    }
+
+    @Benchmark
+//    @Fork(value = 1, warmups = 0)
+//    @Measurement(iterations = 20)
+//    @Warmup(iterations = 1000)
+    @BenchmarkMode(Mode.Throughput)
+    public HeadersMultiMap headersMultiMapSet() {
+        int loopSize = headerNames.length;
+        for (int i=0; i < loopSize; i++) {
+            this.hmm.set(headerNames[i], headerValues[i]);
+        }
+        return this.hmm;
+    }
+
+    @Benchmark
+//    @Fork(value = 1, warmups = 0)
+//    @Measurement(iterations = 20)
+//    @Warmup(iterations = 1000)
+    @BenchmarkMode(Mode.Throughput)
+    public ByteBuf pooledByteBufAllocatorNewDirectBuffer() {
+        ByteBuf buf =  PooledByteBufAllocator.DEFAULT.directBuffer(8,8); // calls newDirectBuffer
         return buf;
     }
 
@@ -76,17 +104,14 @@ public class Benchmarks {
     This seems like a special case since it only has many inlined callees when in the  io.netty.channel.AbstractChannelHandlerContext::invokeWrite0 stack
      */
     @Benchmark
-    @Fork(value = 1, warmups = 1)
+//    @Fork(value = 1, warmups = 0)
+//    @Measurement(iterations = 20)
+//    @Warmup(iterations = 1000)
     @BenchmarkMode(Mode.Throughput)
-    public ByteBuf headersMultiMapEncoderHeader(Blackhole blackhole) {
-        // Buffer will be written to
-        ByteBuf buf =  PooledByteBufAllocator.DEFAULT.directBuffer();
-        HeadersMultiMap hmm = HeadersMultiMap.httpHeaders();
-        for (int i=0; i < headerNames.length; i++) {
-            blackhole.consume(hmm.add(headerNames[i], headerValues[i]));
-        }
+    public ByteBuf headersMultiMapEncoderHeader() {
         // calls encoderHeader
-        hmm.encode(buf);
+        buf.clear();
+        this.hmm.encode(buf);
         return buf;
     }
 }
